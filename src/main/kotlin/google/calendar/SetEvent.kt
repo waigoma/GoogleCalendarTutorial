7535afffe365b78304699c7ad316c9cd10821c17
waigoma <waichi0302@gmail.com>
2022-05-31 12:33:20 +0900

    update SetEvent

--- src/main/kotlin/google/calendar/SetEvent.kt
+++ src/main/kotlin/google/calendar/SetEvent.kt
@@ -1,5 +1,30 @@
 package google.calendar
 
-class SetEvent {
+import Main
+import com.google.api.client.util.DateTime
+import com.google.api.services.calendar.model.Event
+import com.google.api.services.calendar.model.EventDateTime
 
+class SetEvent {
+    companion object {
+        fun createEvent(title: String, description: String="", start: DateTime, end: DateTime): Event {
+            val startTime = EventDateTime().setDateTime(start)
+            val endTime = EventDateTime().setDateTime(end)
+
+            return Event()
+                .setSummary(title)
+                .setDescription(description)
+                .setStart(startTime)
+                .setEnd(endTime)
+        }
+
+        fun createDateTime(year: String, month: String, day: String, hour: String, minute: String): DateTime {
+            // yyyy-MM-ddThh:mm:00+09:00
+            return DateTime("$year-$month-${day}T$hour:$minute:00+09:00")
+        }
+
+        fun registerEvent(event: Event) {
+            Main.service.events().insert(Main.calendarAddress, event).execute()
+        }
+    }
 }
