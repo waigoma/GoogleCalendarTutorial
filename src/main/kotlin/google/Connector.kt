39c0b75a8d124eb9a26e477c0b94b0bd54aa294f
waigoma <waichi0302@gmail.com>
2022-05-31 12:25:47 +0900

    add create method
    接続用の鍵パスを渡すと接続して、メインの変数に格納

--- src/main/kotlin/google/Connector.kt
+++ src/main/kotlin/google/Connector.kt
@@ -1,5 +1,28 @@
 package google
 
-class Connector {
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.CalendarScopes
+import com.google.auth.appengine.AppEngineCredentials
+import com.google.auth.http.HttpCredentialsAdapter
+import java.io.FileInputStream
+import java.util.*
 
+class Connector {
+    companion object {
+        fun create(appName: String, filePath: String): Calendar {
+            val transport = GoogleNetHttpTransport.newTrustedTransport()
+            val credentials = AppEngineCredentials.fromStream(FileInputStream(filePath))
+                .createScoped(Collections.singleton(CalendarScopes.CALENDAR_EVENTS))
+
+            return Calendar.Builder(
+                transport,
+                JacksonFactory.getDefaultInstance(),
+                HttpCredentialsAdapter(credentials)
+            )
+                .setApplicationName(appName)
+                .build()
+        }
+    }
 }
