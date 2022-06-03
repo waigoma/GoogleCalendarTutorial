b8b8f8435841d793bb089fbf76ad320d7d032105
waigoma <waichi0302@gmail.com>
2022-06-03 09:58:33 +0900

    add n days later method

--- src/main/kotlin/com/waigoma/google/DateTimeManager.kt
+++ src/main/kotlin/com/waigoma/google/DateTimeManager.kt
@@ -1,12 +1,50 @@
 package com.waigoma.google
 
 import com.google.api.client.util.DateTime
+import java.util.Calendar
 
 class DateTimeManager {
     companion object {
+        /**
+         * Converts a String to Google DateTime.
+         *
+         * @param year String representation of the year.
+         * @param month String representation of the month.
+         * @param day String representation of the day.
+         * @param hour String representation of the hour.
+         * @param minute String representation of the minute.
+         *
+         * @return Google DateTime object
+         */
         fun create(year: String, month: String, day: String, hour: String, minute: String): DateTime {
             // yyyy-MM-ddThh:mm:00+09:00 (JST)
             return DateTime("$year-$month-${day}T$hour:$minute:00+09:00")
+        }
+
+        /**
+         * Get the date n days from today and get the range of that date
+         *
+         * @param n n days later from today
+         *
+         * @return n days later from today DateTimeRange objects
+         */
+        fun getNDaysLaterFromTodayStartEnd(n: Int): Pair<DateTime, DateTime> {
+            val start = getNDaysLaterFromToday(n)
+            val end = getNDaysLaterFromToday(n, "23", "59")
+            return Pair(start, end)
+        }
+
+        /**
+         * Get the date n days from today.
+         *
+         * @param n n days later from today
+         *
+         * @return n days later from today DateTime object
+         */
+        private fun getNDaysLaterFromToday(n: Int, hour: String="00", minute: String="00"): DateTime {
+            val now = Calendar.getInstance()
+            now.add(Calendar.DATE, n)
+            return create(now.get(Calendar.YEAR).toString(), (now.get(Calendar.MONTH)).toString(), now.get(Calendar.DATE).toString(), hour, minute)
         }
     }
 }
